5bf353c5019ce2fa033be2c7bc63a16211f5d13c
BalderdashStudios <h.ray.emerald@gmail.com>
2024-02-09 13:14:15 -0800

    Added Basic functionality + Tracked files

---
+++ myscript.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	$("#imagetoggler").click(function(){
+		$("img").toggle();
+	});
+	$("h1").click(function() {
+		$("p").css({"color":"red", "background-color": "yellow"});
+		$("img").css("border","25px solid black");
+	});
+	
+	$("#p1").click(function() {
+		$(".disappear").toggle();
+	});
+	
+	$("#scaler").mouseenter(function() { 
+		$(this).css("font-size", "250%");
+	});
+	
+	$("#scaler").mouseleave(function() { 
+		$(this).css("font-size", "200%");
+	});
+	
+	
+});
