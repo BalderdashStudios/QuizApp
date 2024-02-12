2ce32fe5294c2271084cb53c55033e7e9707f8fa
BalderdashStudios <h.ray.emerald@gmail.com>
2024-02-12 14:21:02 -0800

    Added more questions plus wrong answers

--- myscript.js
+++ myscript.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
-	});
-	$("h1").click(function() {
-		$("p").css({"color":"red", "background-color": "yellow"});
-		$("img").css("border","25px solid black");
-	});
+	})
 	
 	$("#p1").click(function() {
 		$(".disappear").toggle();
@@ -21,6 +17,47 @@ $(document).ready(function() {
 	
 	$("#Q1Y").click(function(){
 		$("#Q2").toggle();
+	});
+	
+	$("#Q1N").click(function(){
+		$("#Q1F").toggle();
+	});
+	
+	
+	$("#Q2Y").click(function(){
+		$("#Q3").toggle();
+	});
+	
+	$("#Q2N").click(function(){
+		$("#Q2F").toggle();
+	});
+	
+	$("#Q3Y").click(function(){
+		$("#Q4").toggle();
+	});
+	
+	$("#Q3N").click(function(){
+		$("#Q3F").toggle();
+	});
+	
+	$("#Q3N1").click(function(){
+		$("#Q3F").toggle();
+	});
+	
+	$("#Q4Y").click(function(){
+		$("#Q5").toggle();
+	});
+	
+	$("#Q4N").click(function(){
+		$("#Q4F").toggle();
+	});
+	
+	$("#Q5Y").click(function(){
+		$("#Finish").toggle();
+	});
+	
+	$("#Q5N").click(function(){
+		$("#Q5F").toggle();
 	});
 	
 	
